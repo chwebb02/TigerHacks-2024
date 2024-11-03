9f8925d7557c867c97b3d3e35f15a551053d99e8
Braedennnnn <149883938+Braedennnnn@users.noreply.github.com>
2024-11-02 20:15:45 -0500

    saving myself

--- assets/InGameUI/player_1_tool.gd
+++ assets/InGameUI/player_1_tool.gd
@@ -4,6 +4,7 @@ extends Sprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	eggSplat()
 	pass
 	
 func changeTool(tool: String):
@@ -11,3 +12,9 @@ func changeTool(tool: String):
 		_animations.play(tool + "2")
 	else:
 		print("AnimationPlayer node is null")
+		
+func eggSplat():
+	if $EggSplat:
+		$EggSplat.visible = true
+		$EggSplat.frame = 5
+	
